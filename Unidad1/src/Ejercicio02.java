7eb31a41e989560f9dca4803fde6d070fcdcc630
Robert Gutiérrez <49993075+JRobertGutierrez@users.noreply.github.com>
2021-08-19 01:33:59 +0200

    Add files via upload

--- Unidad1/src/Ejercicio02.java
+++ Unidad1/src/Ejercicio02.java
@@ -4,9 +4,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Programa que calcule el número de número de kilómetros correspondientes a un
- * número N de millas marinas introducidas por teclado. (El número de metros de
- * una milla marina es de 1852).
+ * Programa que calcule el número de kilómetros correspondientes a un número N
+ * de millas marinas introducidas por teclado. (El número de metros de una milla
+ * marina es de 1852).
  * 
  * @author Robert
  *
@@ -21,7 +21,7 @@ public class Ejercicio02 {
 		float millas = 0.0f;
 
 		try {// Controlo errores para que no caiga el programa
-			// Pedimos que el usuario introduzca las millas
+				// Pedimos que el usuario introduzca las millas
 			Scanner s = new Scanner(System.in);
 			System.out.println("Introduzca el número de millas para pasar a kilómetros:");
 
@@ -30,14 +30,14 @@ public class Ejercicio02 {
 
 			// Pasamos las millas a kilómetros
 			km = (millas * 1852) / 1000;
-			
+
 			// Redondeo a 2 decimales con Math.round
-			//kmR = (float) (Math.round(km * 100.0) / 100.0);
-			
+			// kmR = (float) (Math.round(km * 100.0) / 100.0);
+
 			// Redondeo a 3 decimales con BigDecimal y RoundingMode
 			BigDecimal bd = new BigDecimal(km).setScale(3, RoundingMode.HALF_UP);
 			kmR = (float) bd.doubleValue();
-			
+
 			// Mostramos el resultado final
 			System.out.println(millas + " Millas son " + kmR + " kilómetros.");
 		} catch (InputMismatchException e) {// Informo de la excepción
